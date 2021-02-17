ef4a77dad1db9fbbc85c3e80835bc57ae6985496
mariele <mariele_rbr@hotmail.com>
2021-02-16 21:41:26 -0300

    Conceitos React/Configurando Babel

---
+++ public/script.js
@@ -0,0 +1,7 @@
+/**
+ * Babel => Converter ( transpilar ) código do React para um código que o browser entenda
+ * Webpack => Pra cada tipo de arquivo (.js, .css, .png) eu vou converter o codigo de uma maneira diferente
+ *
+ * Loader: Babel-loader, css-loader, image-loader
+ *  yarn add @babel/core @babel/preset-env @babel/preset-react webpack webpack-cli
+ */
