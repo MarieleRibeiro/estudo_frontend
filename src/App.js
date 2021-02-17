deca91a1eddaf2ed83c038d11c74b0b570725b6a
mariele <mariele_rbr@hotmail.com>
2021-02-17 15:22:10 -0300

    Propriedades

--- src/App.js
+++ src/App.js
@@ -8,13 +8,32 @@ import Header from './components/Header'
 <> </> => conceito de fragment = quando quero criar um container pra envolver 1 ou mais components sem reproduzir efeito na DOM( arvore de elementos )
 */
 
+
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects" >
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   )
 }
 
 export default App;
+
+/**
+ * 3 CONCEITOS MAIS IMPORTANTES DO REACT
+ * => Componente
+ * => Propriedades = é alguma informação que podemos passar de componente Pai para um componente Filho
+ * => Estado
+ */
