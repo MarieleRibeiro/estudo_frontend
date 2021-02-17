89806d30c03edcafa4f5a9fbaadefd94f44eb17f
mariele <mariele_rbr@hotmail.com>
2021-02-17 16:01:58 -0300

    Estado e Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header'
 
 /* Sempre que for criado um componente no react coloca sempre com letra maiuscula
@@ -10,21 +10,32 @@ import Header from './components/Header'
 
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+
+  /**
+        useState retorna um array com 2 posições
+        => 1. varíavel com o valor inicial
+        => 2. Função para atualizarmos esse valor
+   */
+
+
+  function handleAddproject() {
+    // projects.push(`Novo pojeto ${Date.now()}`) (metodo push altera o valor original não cria um novo array com a nova informação)
+
+    setProjects([...projects, `Novo pojeto ${Date.now()}`]);
+
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects" >
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddproject}>Adicionar novo projeto</button>
+
     </>
   )
 }
@@ -35,5 +46,5 @@ export default App;
  * 3 CONCEITOS MAIS IMPORTANTES DO REACT
  * => Componente
  * => Propriedades = é alguma informação que podemos passar de componente Pai para um componente Filho
- * => Estado
+ * => Estado & imutabilidade (useState=> utilizar estado )
  */
